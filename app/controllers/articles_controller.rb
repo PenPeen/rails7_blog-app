ce38b353baf80caec2f64356146a42d81e6499b7
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-09-16 15:27:11 +0900

    環境構築及び移行

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+
+    # 記事の一覧表示
+    def index
+        @articles = Article.all
+    end
+
+    # 記事の詳細表示
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    # 記事の作成画面
+    def new
+        @article = Article.new
+    end
+
+    #　記事の作成処理
+    def create
+        Article.create(article_params)
+        redirect_to articles_path
+    end
+
+    # 記事の編集
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    # 記事の更新
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params);
+
+        redirect_to articles_path
+    end
+
+    # 記事削除
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path, status: :see_other
+    end
+
+    #　ストロングパラメータ
+    private
+        def article_params
+            params.require('article').permit('title', 'body');
+        end
+end
